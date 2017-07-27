638eff228e66236a4ca9180dfbe0c23081000e56
Eric Sproles <tron04@gmail.com>
2017-07-26 17:48:38 -0700

    updated package.son and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-module.exports = {
+
+// NODE_ENV to production
+// uglify
+
+var config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -26,3 +31,17 @@ module.exports = {
     })
   ]
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    // Set node env inside the code webpack complies for us
+    new webpack.DefinePlugin({
+      'process.env' : {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
+}
+
+module.exports = config
